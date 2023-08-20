7272f5984e55cc99208a56a4656593208b5a4cc2
oshaniwickramasinghe <84929391+oshaniwickramasinghe@users.noreply.github.com>
2023-08-20 20:52:18 +0530

    sync

---
+++ src/entity/FlightClass.ts
@@ -0,0 +1,31 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn } from "typeorm"
+
+@Entity()
+export class Planet {
+
+    @PrimaryGeneratedColumn()
+    planetId: number
+
+    @Column()
+    planetName: string
+
+    @Column("blob") 
+    image: Buffer;
+
+    @Column()
+    description: string
+
+    @OneToOne(() => Spaceport, spaceport => spaceport.planet) 
+    @JoinColumn()
+    spaceport: Spaceport; 
+
+    @Column({ nullable: true })
+    spaceportId: number; 
+
+    constructor(planetName: string, image: Buffer, description: string) {
+
+        this.planetName = planetName
+        this.image = image
+        this.description = description
+    }
+}
